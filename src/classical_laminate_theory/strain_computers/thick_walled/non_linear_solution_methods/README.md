6b4ab1fc92c96e1d990a9c874592cf7b5f6a6536
Victor Poorte <victorpoorte@gmail.com>
2025-07-23 13:40:31 +0200

    Remove strain computers directory
    These actually belong to the cylinder module.

--- src/classical_laminate_theory/strain_computers/thick_walled/non_linear_solution_methods/README.md
+++
@@ -1,2 +0,0 @@
-# non_linear_solution_methods
-Methods to solve system of non linear equations
--- src/classical_laminate_theory/strain_computers/thick_walled/non_linear_solution_methods/__init__.py
+++
@@ -1,6 +0,0 @@
-from .non_linear_solution_methods.convergence_checkers import (
-    ForceResidualConvergence,
-)
-from .non_linear_solution_methods.newton_raphson_with_residuals import (
-    NewtonRaphsonWithResiduals,
-)
